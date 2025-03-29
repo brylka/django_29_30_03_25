f5993160b0a9976f2c08a9855cd4fa16622de87f
Bartosz Bryniarski <brylka@jelenia.com>
2025-03-29 10:25:36 +0100

    Sobota

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,4 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request):
-    return HttpResponse("Hello World!")
+    print("Moje komentarze")
+    return render(request, 'myapp/hello_world.html')
+    #return HttpResponse("<style> body {background-color: red; color: white;}</style><h1>Hello World!</h1><hr><p>Moja aplikacja</p>")
