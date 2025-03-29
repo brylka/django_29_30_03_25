b14b4b3e6a95044c6704fd153d463dcad0092a1c
Bartosz Bryniarski <brylka@jelenia.com>
2025-03-29 13:36:13 +0100

    Sobota

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import hello_world, hello_name, add, div
+from .views import hello_world, hello_name, add, div, table
 
 urlpatterns = [
     path('hello/', hello_world),
     path('hello/<str:name>/', hello_name),
     path('add/<int:a>/<int:b>/', add),
     path('div/<int:a>/<int:b>/', div),
+    path('tables/', table)
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -20,3 +20,22 @@ def div(request, a, b):
     if b != 0:
         context['c'] = a / b
     return render(request, 'myapp/div.html', context)
+
+def table(request):
+    posts = [
+        {'title': 'Post pierwszy', 'content': 'Terść pierwszego posta', 'created_at': '2025-03-10'},
+        {'title': 'Post drugi  ', 'content': 'Terść drugiego posta', 'created_at': '2025-03-13'},
+        {'title': 'Post trzeci  ', 'content': 'Terść trzeciego posta', 'created_at': '2025-03-16'},
+        {'title': 'Post czwarty', 'content': 'Terść czwartego posta', 'created_at': '2025-03-20'},
+        {'title': 'Post piąty', 'content': 'Terść piątego posta', 'created_at': '2025-03-24'},
+    ]
+
+    # text = ''
+    # for post in posts:
+    #     text += (f"{post['title']}\t\t\t{post['content']}\t\t{post['created_at']}<br>\n")
+    return render(request, 'myapp/tables.html', {'posts': posts})
+    #return HttpResponse(text)
+
+
+if __name__ == '__main__':
+    table()
