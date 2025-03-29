3949e1f1ac4c04cc882fce97ef692dcf3e482867
Bartosz Bryniarski <brylka@jelenia.com>
2025-03-29 10:39:38 +0100

    Sobota

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, hello_name
 
 urlpatterns = [
-    path('hello/', hello_world)
+    path('hello/', hello_world),
+    path('hello/<str:name>/', hello_name),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -5,3 +5,6 @@ def hello_world(request):
     print("Moje komentarze")
     return render(request, 'myapp/hello_world.html')
     #return HttpResponse("<style> body {background-color: red; color: white;}</style><h1>Hello World!</h1><hr><p>Moja aplikacja</p>")
+
+def hello_name(request, name):
+    return HttpResponse(f"Witaj {name}!")
